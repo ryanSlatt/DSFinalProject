e315607c6d229a0d324e5c1b96d8254da9f60476
ryanSlatt <153004015+ryanSlatt@users.noreply.github.com>
2026-04-29 22:41:13 -0400

    Import/Exports Regressions and Lag Changes
    Added regressions for imports and exports, and created regression tables. Added a fourth lag to all models.  Swapped x and y in ccf function so positive values of "lag" on the bar plot represent tariff rate affecting future balance of trade. Updated labels on bar plot.

--- TariffTradeRegression.R
+++ TariffTradeRegression.R
@@ -43,6 +43,7 @@ tariffData = tariffData %>% filter(mfn_ad_val_rate < 100)
 
 
 #Selecting a few variables from tariff data (unlikely to use them all)
+#Comment this line out if making the copy for random forests
 tariffData = tariffData %>% select(year,hts8,mfn_ad_val_rate)
 
 #Handling "duplicates" as discussed above
@@ -51,6 +52,13 @@ tariffData = tariffData %>% group_by(year,hts8) %>% summarise(adValRate = mean(m
 data = full_join(tradeData,tariffData, join_by("Year"=="year","HTS8"=="hts8"))
 data$adValRate = data$adValRate %>% replace_na(0) #Omitting this step results in a positive coef in the regression. But it seems to be a reasonable approach to me - anything that doesn't match up clearly has a 0% tariff rate.
 
+#Creating a less cleaned up version for use with random forests in Python
+# data$mfn_ad_val_rate = data$mfn_ad_val_rate %>% replace_na(0) #Omitting this step results in a positive coef in the regression. But it seems to be a reasonable approach to me - anything that doesn't match up clearly has a 0% tariff rate.
+# write.csv(data,"MLReadyCombinedDataset.csv")
+# write.csv(countryData,"countryData.csv")
+
+#Creating a series ID for use with feols function (needed when using lags)
+data = data %>% group_by(Country,HTS8) %>% mutate(seriesID = cur_group_id())
 
 #Aggregating to country level (adding across HTS)
 countryData = data %>% group_by(Year,Country) %>% summarise(tradeBalance = sum(tradeBalance,na.rm=TRUE),
@@ -65,16 +73,18 @@ globalData = countryData %>% group_by(Year) %>% summarise(tradeBalance = sum(tra
                                                             adValRate = sum(Imports*adValRate,na.rm=TRUE)/sum(Imports,na.rm=TRUE))
 
 #Unscaled regressions
-unscaledProductLevelFE = feols(data, tradeBalance ~ adValRate | Year + Country + HTS8)
-unscaledCountryLevelFE = feols(countryData, tradeBalance ~ adValRate | Year + Country)
-globalLevelModel = feols(globalData, tradeBalance~adValRate)
+unscaledProductLevelFE = feols(data, tradeBalance ~ l(adValRate,0:4) | Year + Country + HTS8, panel.id = ~seriesID+Year)
+unscaledCountryLevelFE = feols(countryData, tradeBalance ~ l(adValRate,0:4) | Year + Country,panel.id = ~Country+Year)
+globalLevelModel = lm(tradeBalance~adValRate + lag(adValRate)+ lag(adValRate,2)+ lag(adValRate,3)+ lag(adValRate,4),globalData)
 
 #Scale each over time and repeat regressions
-data = data %>% group_by(Country,HTS8) %>% mutate(tradeBalance = scale(tradeBalance))
+data = data %>% group_by(Country,HTS8) %>% mutate(tradeBalance = scale(tradeBalance),Exports = scale(Exports),Imports = scale(Imports))
 scaledProductLevelFE = feols(data, tradeBalance ~ adValRate | Year + Country + HTS8)
 
-countryData = countryData %>% group_by(Country) %>% mutate(tradeBalance = scale(tradeBalance))
+
+countryData = countryData %>% group_by(Country) %>% mutate(tradeBalance = scale(tradeBalance),Exports = scale(Exports),Imports = scale(Imports))
 scaledCountryLevelFE = feols(countryData, tradeBalance ~ adValRate | Year + Country)
+
 
 rm(tariffData)
 rm(tradeData)
@@ -92,16 +102,40 @@ rm(tradeData)
 
 
 #Lag regressions
-countryLevelLags =  feols(countryData, tradeBalance ~ l(adValRate,0:3) | Year + Country, panel.id = ~Country+Year)
+countryLevelLags =  feols(countryData, tradeBalance ~ l(adValRate,0:4) | Year + Country, panel.id = ~Country+Year)
 countryLevelLags
+countryLevelLagsExports =  feols(countryData, Exports ~ l(adValRate,0:4) | Year + Country, panel.id = ~Country+Year)
+countryLevelLagsImports =  feols(countryData, Imports ~ l(adValRate,0:4) | Year + Country, panel.id = ~Country+Year)
 
 
 data = data %>% group_by(Country,HTS8) %>% mutate(seriesID = cur_group_id())
-productLevelLags =  feols(data, tradeBalance ~ l(adValRate,0:3) | Year + Country + HTS8, panel.id = ~seriesID+Year)
+productLevelLags =  feols(data, tradeBalance ~ l(adValRate,0:4) | Year + Country + HTS8, panel.id = ~seriesID+Year)
 productLevelLags
+productLevelLagsExports =  feols(data, Exports ~ l(adValRate,0:4) | Year + Country + HTS8, panel.id = ~seriesID+Year)
+productLevelLagsImports =  feols(data, Imports ~ l(adValRate,0:4) | Year + Country + HTS8, panel.id = ~seriesID+Year)
+
+
 
 #Creating tables for the models
-modelsummary(stars = c("*" = .05, "**" = .01, "***" = 0.001), list("HTS8-Level" = productLevelLags, "Country-Level   " =countryLevelLags),output = "regressionTable.png")
+#Balance of Trade Table
+
+coef_map = c(
+  'l(adValRate, 0)'= "lag 0",
+  'l(adValRate, 1)'= "lag 1",
+  'l(adValRate, 2)'= "lag 2",
+  'l(adValRate, 3)'= "lag 3",
+  'l(adValRate, 4)'= "lag 4"
+)
+
+#modelsummary(stars = c("*" = .05, "**" = .01, "***" = 0.001), list("HTS8-Level" = productLevelLags, "Country-Level   " =countryLevelLags))
+modelsummary(stars = c("*" = .05, "**" = .01, "***" = 0.001), list("HTS8-Level" = productLevelLags, "Country-Level   " =countryLevelLags),coef_map = coef_map,output = "regressionTable.png")
+
+#Exports Table
+modelsummary(stars = c("*" = .05, "**" = .01, "***" = 0.001), list("HTS8-Level" = productLevelLagsExports, "Country-Level   " =countryLevelLagsExports),coef_map = coef_map,output = "exportsRegressionTable.png")
+
+#Imports Table
+modelsummary(stars = c("*" = .05, "**" = .01, "***" = 0.001), list("HTS8-Level" = productLevelLagsImports, "Country-Level   " =countryLevelLagsImports),coef_map = coef_map,output = "importsRegressionTable.png")
+
 
 #Removing NA country observations. The regressions drop them automatically but ccf does not
 countryData = countryData %>% filter(!is.na(Country) & !is.na(Year))
@@ -117,7 +151,7 @@ for (c in unique(countryData$Country)){
   tempData = countryData %>% filter(Country == c)
   x = ts(tempData$adValRate)
   y = ts(drop(tempData$tradeBalance))
-  ccfTest = ccf(x,y,na.action = na.pass,plot = FALSE)
+  ccfTest = ccf(y,x,na.action = na.pass,plot = FALSE) #Swapped x,y to y,x to make it so the positive lags on the plot match up with ad valorem tariff rate now affecting balance of trade in the future
   results = ccfTest$acf
   lagLabels = ccfTest$lag
   signficantLags = (results < -1.96/sqrt(length(x)) | results > 1.96/sqrt(length(x)))
@@ -130,4 +164,4 @@ for (c in unique(countryData$Country)){
 #Summing across time period to get the number of significant lags in each time period
 summedLags = colSums(lagsTable,na.rm=TRUE)
 summedLags = data.frame(summedLags)
-barplot(summedLags$summedLags,names.arg = columns,ylab = "Number of Countries with a \"Significant\" Lag", xlab = "Time period", main ="Number of \"Significant\" Lags per time period")
+barplot(summedLags$summedLags,names.arg = columns,ylab = "Number of Significant Lags", xlab = "Lag", main = "Bar Plot of Significant Lags Across Country")
